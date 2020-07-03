b6256b9e0e3f60f5391be69d757ecc765a0c1ea6
Михаил <sazanof.ru@yandex.ru>
2020-07-03 15:08:16 +0300

    up

--- domainVars.snippet.php
+++ domainVars.snippet.php
@@ -9,6 +9,24 @@ require_once __DIR__ . '/autoload.php';
         $modx->config['site_url'] = $d->toDomainName($ex[0]);
         $modx->setPlaceholder('base_href',$d->toDomainName($ex[0]));
         //echo $d->toDomainName($ex[0]);
-        var_dump($modx->placeholders);
+
+        $vars = $d->getGlobalVars($domain['id']);
+        $plh = array();
+        foreach ($vars as $var){
+            if($var['value_value'] == ''){
+                $var['v'] = $var['default_value'];
+            }
+            else {
+                $var['v'] = $var['value_value'];
+            }
+            // типы переменной (текст. чанки и тд)
+            switch (intval($var['type'])){
+                case 1:
+                    break;
+            }
+            $plh[$var['key']] = $var;
+        }
+        $modx->toPlaceholders($plh,'domain.');
+        echo '<pre>';print_r($modx->placeholders);echo '</pre>';
     }
 
